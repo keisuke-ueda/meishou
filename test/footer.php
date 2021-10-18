f8a30fc5d5b82b7314d6487b8bbd0e4d4f214a92
hara <nov17hk@gmail.com>
2021-10-18 21:05:51 +0900

    ワードプレス組み替え

---
+++ test/footer.php
@@ -0,0 +1,39 @@
+<footer class="container mt-5 pb-5 border-top">
+      <div class="row">
+        <div class="mt-3 col-md-4 text-light bg-dark text-center">
+          <h3>会社情報</h3>
+
+          <div class="text-center">
+            <p>〒132-0015</p>
+            <p>東京都江戸川区西瑞ピュアホワイトB201 <br /></p>
+          </div>
+        </div>
+
+        <div class="mt-3 col-md-4 text-light bg-dark text-center">
+          <h3>お問い合わせ</h3>
+          <p>CONTACT</p>
+          <div class="d-flex justify-content-center">
+            <img src="<?php echo get_template_directory_uri();?>/img/tel.png" alt="" class="tel" width="30px" height="30px" />
+            <p>お気軽にお問い合わせください</p>
+          </div>
+          <h1>090-4628-1074</h1>
+          <a href="" class="btn btn--dark btn--radius">メール送信フォームへ</a>
+        </div>
+        <div class="mt-3 col-md-4 text-light text-center">
+          <p>こちらから検索ください</p>
+          <div class="input-group">
+            <input type="search" class="form-control me-2" placeholder="テキスト入力欄" />
+            <span class="input-group-btn">
+              <button type="button" class="btn btn-default">検索</button>
+            </span>
+          </div>
+        </div>
+      </div>
+    </footer>
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
+      crossorigin="anonymous"
+    ></script>
+    <?php wp_footer(); ?>
---
+++ test/page-design.php
@@ -0,0 +1,56 @@
+<?php
+/*
+Template Name: デザインページ
+*/
+?>
+
+<?php get_header(); ?>
+    <main class="container mt-5">
+      <div class="row text-center text-light border-bottom border-2">
+        <h1>デザインイメージ集</h1>
+      </div>
+
+      <a href="" class="design">
+        <div class="row mt-5 pb-5 align-items-center border-bottom border-2">
+          <div class="col-md-6">
+            <h3 class="text-light text-center border-bottom">デザインイメージ1</h3>
+            <h5 class="text-light">
+              ここに説明が入りますここに説明が入りますここに説明が入りますここに説明が入りますここに説明が入りますここに説明が入りますここに説明が入りますここに説明が入ります
+            </h5>
+          </div>
+          <div class="col-md-6 d-flex justify-content-center border">
+            <img src="<?php echo get_template_directory_uri();?>/img/logo1.png" alt="" width="50%" height="50%" />
+          </div>
+        </div>
+      </a>
+
+      <a href="" class="design">
+        <div class="row mt-5 pb-5 align-items-center border-bottom border-2">
+          <div class="col-md-6">
+            <h3 class="text-light text-center border-bottom">デザインイメージ2</h3>
+            <h5 class="text-light">
+              ここに説明が入りますここに説明が入りますここに説明が入りますここに説明が入りますここに説明が入りますここに説明が入りますここに説明が入りますここに説明が入りますここに説明が入ります
+            </h5>
+          </div>
+          <div class="col-md-6 d-flex justify-content-center border">
+            <img src="<?php echo get_template_directory_uri();?>/img/logo1.png" alt="" width="50%" height="50%" />
+          </div>
+        </div>
+      </a>
+
+      <a href="" class="design">
+        <div class="row mt-5 pb-5 align-items-center">
+          <div class="col-md-6">
+            <h3 class="text-light text-center border-bottom">デザインイメージ3</h3>
+            <h5 class="text-light">
+              ここに説明が入りますここに説明が入りますここに説明が入りますここに説明が入りますここに説明が入りますここに説明が入りますここに説明が入りますここに説明が入りますここに説明が入ります
+            </h5>
+          </div>
+          <div class="col-md-6 d-flex justify-content-center border">
+            <img src="<?php echo get_template_directory_uri();?>/img/logo1.png" alt="" width="50%" height="50%" />
+          </div>
+        </div>
+      </a>
+    </main>
+
+    <?php get_footer(); ?>
